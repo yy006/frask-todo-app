8bd9e07f32bcb7c0b1e87517e6f0d45a99902ffb
yy006 <y1u2i3t4o@icloud.com>
2023-05-19 16:34:26 +0900

    test
    test

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, redirect # 追加で2つimportする
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    detail = db.Column(db.String(100))
+    due = db.Column(db.DateTime, nullable=False)
+
+# @app.route('/') # 元々はこれだったけど
+@app.route('/', methods=['GET', 'POST']) # こちらに変更
+def index():
+    if request.method == 'GET':
+        posts = Post.query.all()
+        return render_template('index.html', posts=posts)
+
+    else:
+        title = request.form.get('title')
+        detail = request.form.get('detail')
+        due = request.form.get('due')
+        
+        due = datetime.strptime(due, '%Y-%m-%d')
+        new_post = Post(title=title, detail=detail, due=due)
+
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/')
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+@app.route('/detail/<int:id>')
+def read(id):
+    post = Post.query.get(id)
+
+    return render_template('detail.html', post=post)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    post = Post.query.get(id)
+
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/')
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    post = Post.query.get(id)
+    if request.method == 'GET':
+        return render_template('update.html', post=post)
+    else:
+        post.title = request.form.get('title')
+        post.detail = request.form.get('detail')
+        post.due = datetime.strptime(request.form.get('due'), '%Y-%m-%d')
+
+        db.session.commit()
+        return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
